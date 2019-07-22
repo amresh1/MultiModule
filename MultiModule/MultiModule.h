094617b204a79c4fbbb418900d3589ae0ce729f8
Amresh Subedi <amresh.subedi1@gmail.com>
2019-07-22 15:35:11 +0545

    Initial Commit

---
+++ MultiModule/MultiModule.h
@@ -0,0 +1,19 @@
+//
+//  MultiModule.h
+//  MultiModule
+//
+//  Created by Amresh Subedi on 7/22/19.
+//  Copyright © 2019 Aavishkar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MultiModule.
+FOUNDATION_EXPORT double MultiModuleVersionNumber;
+
+//! Project version string for MultiModule.
+FOUNDATION_EXPORT const unsigned char MultiModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MultiModule/PublicHeader.h>
+
+
